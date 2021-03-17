c8e003c3e503b874416f6f101f88bcf34af456a4
Steve McNamara <stephenmcnamara36@gmail.com>
2021-03-16 19:09:26 -0600

    added api and html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,19 @@
 // create html routes
+const db = require("../models");
+const path = require("path");
 
-//html route for exercise.html
+module.exports = (app) => {
+  //html route for exercise.html
+  app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
 
-//html route for index.html
-
-//html route for stats.html
+  //html route for index.html
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+  //html route for stats.html
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+};
